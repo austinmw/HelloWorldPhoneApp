a65f7147d73d5c32b74a9b86678157043452db21
austinmw <austinmw89@gmail.com>
2024-09-18 12:33:24 +0800

    Initial Commit

---
+++ HelloWorldPhoneApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelloWorldPhoneApp
+//
+//  Created by Austin Welch on 9/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HelloWorldPhoneApp/HelloWorldPhoneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldPhoneAppApp.swift
+//  HelloWorldPhoneApp
+//
+//  Created by Austin Welch on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldPhoneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
